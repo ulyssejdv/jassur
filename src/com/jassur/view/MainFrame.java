3d48f4fcf428fcd1c73e3069510fca8420d20512
ulyssejdv <u.devillartay@gmail.com>
2016-05-09 15:16:03 +0200

    bug fix : rafraichissement de la main frame

--- src/com/jassur/view/MainFrame.java
+++ src/com/jassur/view/MainFrame.java
@@ -23,10 +23,12 @@ public class MainFrame extends JFrame {
 	private static MainFrame INSTANCE = null;
 	
 	public static MainFrame getInstance(JPanel p) {
-		if (INSTANCE != null) {
+		/*if (INSTANCE != null) {
 			INSTANCE.dispose();
-		}
+		}*/
+		MainFrame tmpMf = INSTANCE;
 		INSTANCE = new MainFrame(p);
+		tmpMf.dispose();
 		return INSTANCE;
 	}
 	
@@ -39,7 +41,7 @@ public class MainFrame extends JFrame {
 		setTitle("JASSUR - Client");
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 900, 600);
+		setBounds(100, 100, 1024, 800);
 		
 		
 		JPanel contentPane = new JPanel();
