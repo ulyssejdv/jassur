10b52fb995a5782132fa36f876a77169478675cf
Sarah2723 <allouchesarah27@hotmail.fr>
2016-05-18 14:48:40 +0200

    Comparaison des simulations

--- src/simulation_pret/Main.java
+++ src/simulation_pret/Main.java
@@ -9,7 +9,7 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-//METTRE EN ANGLAIS COMMENTAIRES
+//METTRE EN ANGLAIS code et COMMENTAIRES
 
 
 public class Main extends JFrame implements ActionListener {
